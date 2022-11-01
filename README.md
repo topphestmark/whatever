5fec424ccdc1ef65fe1738b754b788cb56e16506
Suphachai Hestmark <top_hestmark@yahoo.no>
2022-11-01 04:06:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#  Typescript
+
+> whatever
+
+## Use
+
+```bash
+npx typescript <name>
+```
+
+Created by [create-whatever](https://github.com/uetchy/create-whatever/blob/master/README.md).
---
+++ src/cli.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { create } from 'create-whatever';
+import { resolve } from 'path';
+
+const templateRoot = resolve(__dirname, '../templates');
+
+const caveat = `
+This is a caveat!
+You can change this in \`src/cli.ts\`.
+`;
+
+// See https://github.com/uetchy/create-whatever/blob/master/README.md for the all options.
+
+create('typescript', {
+  templateRoot,
+  extra: {
+    architecture: {
+      type: 'list',
+      describe: 'choose your fave os',
+      choices: [
+        { name: 'macOS', value: 'mac' },
+        { name: 'Windows', value: 'win' },
+        { name: 'Linux', value: 'linux' },
+      ],
+      prompt: 'if-no-arg',
+    },
+  },
+  after: ({ answers }) => console.log(`Ok you chose ${answers.architecture}.`),
+  caveat,
+});
